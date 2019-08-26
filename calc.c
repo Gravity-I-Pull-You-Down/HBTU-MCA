6312527a4ac18964d9356942b67319c317b92560
gravity <gravity@Gravity.gravity>
2019-08-26 14:51:55 +0530

    C practicle lab 2

---
+++ calc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int firstNumber,secondNumber,choice;
+	float result;
+
+	printf("Enter first number to operate :\n");
+	scanf("%d",&firstNumber);
+	printf("Enter second number\n");
+	scanf("%d",&secondNumber);
+	printf("Enter the operation\n 1 for addition \n 2 for subtraction\n 3 for multiplication \n 4 for division\n");
+	scanf("%d",&choice);
+
+	switch(choice)
+	{
+	case 1:
+	result = firstNumber+secondNumber;
+	break;
+	case 2:
+	result = firstNumber-secondNumber;
+	break;
+	case 3:
+	result = firstNumber*secondNumber;
+	break;
+	case 4:
+	result= firstNumber/secondNumber;
+	break;
+	default:
+	printf("Enter a valid choice\n");
+
+	}
+	printf("the answer is %f\n",result);
+	return 0;
+}
