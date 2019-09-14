2146d96f61529d37ed60c5160dc696c3053bf520
gravity <gravity@Gravity.gravity>
2019-09-14 10:46:09 +0530

    C practicle lab 3

---
+++ poi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int v=5,*pv;
+	int *a = &v;
+	*pv = &v;
+	printf("%d \n %u \n %u \n",v,pv,a );
+	return 0;
+}
---
+++ temp1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	float j = 3.1;
+	printf("%f\n",j );
+	if(j==3.1f)
+		printf("DK\n");
+	else
+		printf("SK\n");
+	return 0;
+}
